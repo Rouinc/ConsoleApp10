2bb30316d148972f9088afdfa754fb112d7679c8
Rouinc <vincentalex9@outlook.com>
2021-12-03 10:47:54 +0700

    cek prima atau bukan

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int angka;
+
+        Console.WriteLine("Input angka");
+        angka = Convert.ToInt32(Console.ReadLine());
+        if (angka == 0 || angka == 1)
+        {
+            Console.WriteLine(angka + " bukan bilangan prima");
+            Console.ReadLine();
+        }
+        else
+        {
+            {
+                if (angka % 2 == 0)
+                {
+                    Console.WriteLine(angka + "  bukan bilangan prima");
+                }
+
+            }
+            {
+                if (angka % 2 != 0)
+                {
+                    Console.WriteLine(angka + "  adalah bilangan prima");
+                }
+            }
+        }
+    }
+}
